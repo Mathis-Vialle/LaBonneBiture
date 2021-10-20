c39d94f3b95d9234bf012b97715d8a1113a55899
Mathis Vialle <mathis.vialle.etudiant@gmail.com>
2021-10-20 14:41:05 +0200

    Tout les fichiers nécéssaires
    La bonne Biture en marche

---
+++ 404.php
@@ -0,0 +1,9 @@
+<?php
+get_header();
+?>
+<h1> 404 </h1> 
+<img src="https://i.ytimg.com/vi/TrEhn02xpII/hqdefault.jpg" alt="Oui, c'est pour ?" aria-label="Oui, c'est pour ?"/>
+<?php
+get_sidebar();
+get_footer();
+?>
